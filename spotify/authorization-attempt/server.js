a7a601dc4ca8d4e6a087824b2676597144db3195
Jeff Lapp <lappjeff20@gmail.com>
2019-12-05 14:10:47 -0500

    Added cookies and /me endpoint to retrieve logged in user data

--- spotify/authorization-attempt/server.js
+++ spotify/authorization-attempt/server.js
@@ -1,8 +1,8 @@
 const qs = require("query-string");
 const express = require("express");
 const axios = require("axios");
+const SpotifyWebApi = require("spotify-web-api-node");
 require("dotenv").config();
-
 const app = express();
 
 app.use(express.json());
@@ -10,6 +10,12 @@ app.use(express.json());
 const scope = "user-read-private user-read-email";
 const client_id = process.env.CLIENT_ID;
 const client_secret = process.env.CLIENT_SECRET;
+
+const spotifyApi = new SpotifyWebApi({
+	client_id,
+	client_secret,
+	redirect_uri: process.env.REDIRECT_URI
+});
 
 app.get("/", async (req, res) => {
 	const params = {
@@ -55,12 +61,29 @@ app.get("/callback", async (req, res) => {
 		} = response.data;
 		const status = response.status;
 
-		res.status(status).json({ accessToken, refreshToken });
+		res
+			.status(status)
+			.cookie("spotifyAccessToken", accessToken, { maxAge: 34000 })
+			.json({ accessToken, refreshToken });
 	} catch (err) {
 		res.status(err.response.status).json({ message: err.response.statusText });
 	}
 });
 
+app.get("/me", async (req, res) => {
+	try {
+		spotifyApi.setAccessToken(
+			"BQDnvqZwlXVH4EwT2tfnJd28ms6PXlbNMbS4REz75qqlJqQN99DVQCGPP1njGcCDQIdaueUbF-Etj2VqLGKbR7E5pelwOsmae2_ArDAB452RiHeZIOaweCkWJLJLNsud-Wczs3eN28tHTAtb7XK5m8v1OSv2eMI38A"
+		);
+		const me = await spotifyApi.getMe();
+		console.log(me.body);
+
+		res.status(200).json({ ...me.body });
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({ message: err });
+	}
+});
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.info(`Server running on port ${port}`));

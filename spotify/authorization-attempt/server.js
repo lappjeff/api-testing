f9b132e56d75ecb8516abb48fa98a7f5a5c6f815
Jeff Lapp <lappjeff20@gmail.com>
2019-12-05 10:25:22 -0500

    Initialized server successfully

--- spotify/authorization-attempt/server.js
+++ spotify/authorization-attempt/server.js
@@ -1,2 +1,10 @@
 const express = require("express");
 require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.info(`Server running on port ${port}`));
